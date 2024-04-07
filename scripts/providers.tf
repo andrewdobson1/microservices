bc22da2e69717e6f092466d5ee6e6f6ee684b3bf
andrewdobson1 <andrewdobson@outlook.com>
2024-04-07 17:42:37 +0100

    .

--- scripts/providers.tf
+++ scripts/providers.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.98.0"
     }
+    tls = {
+      source = "hashicorp/tls"
+      version = "4.0.5"
+    }
   }
 }
 
@@ -13,4 +17,8 @@ provider "azurerm" {
   # Your existing configuration
 
   features {}  # Add this line
+}
+
+provider "tls" {
+  # Configuration options
 }
