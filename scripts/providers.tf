5d7df11d7c04adb8566598d31f0d821221182929
andrewdobson1 <andrewdobson@outlook.com>
2024-04-07 17:18:18 +0100

    updated providers

--- scripts/providers.tf
+++ scripts/providers.tf
@@ -1,5 +1,16 @@
 # Initialises Terraform providers and sets their version numbers.
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.98.0"
+    }
+  }
+}
+
 provider "azurerm" {
-    version = "1.38.0"
+  # Your existing configuration
+
+  features {}  # Add this line
 }
