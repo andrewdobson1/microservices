a625326bdfa168b36a0c46007e293e285f152aff
andrewdobson1 <andrewdobson@outlook.com>
2024-04-07 18:44:09 +0100

    kubernetes

--- scripts/container-registry.tf
+++ scripts/container-registry.tf
@@ -1,9 +1,9 @@
 # Creates a container registry on Azure so that you can publish your Docker images.
 
 resource "azurerm_container_registry" "container_registry" {
-  name                = "crflixtube"
+  name                = var.app_name
   resource_group_name = azurerm_resource_group.flixtube.name
-  location            = "ukwest"
+  location            = var.location
   admin_enabled       = true
   sku                 = "Basic"
 }
@@ -17,6 +17,6 @@ output "registry_un" {
 }
 
 output "registry_pw" {
-  value = azurerm_container_registry.container_registry.admin_password
+  value =   azurerm_container_registry.container_registry.admin_password
   sensitive = true
 }
---
+++ scripts/variables.tf
@@ -0,0 +1,20 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+    default = "dsflixtube"
+}
+variable location {
+  default = "UK West"
+}
+
+variable admin_username {
+  default = "linux_admin"
+}
+
+variable client_id {
+
+}
+
+variable client_secret {
+
+}
